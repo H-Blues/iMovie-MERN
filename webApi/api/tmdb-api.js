492c61f46acec57fbd092695a74b06a5f72fa2ad
Zihan Zhang <hblues419@gmail.com>
2022-12-29 01:29:18 +0000

    feat: add more functions in tmdbAPI

--- webApi/api/tmdb-api.js
+++ webApi/api/tmdb-api.js
@@ -52,3 +52,68 @@ export const getMovieCredits = (id) => {
     throw error;
   });
 };
+
+export const getTopRatedTv = () => {
+  return fetch(
+    `${tmdbUrl}/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      return null;
+    }
+    return response.json();
+  }).catch((error) => {
+    throw error;
+  });
+};
+
+export const getTvReviews = (id) => {
+  return fetch(
+    `${tmdbUrl}/tv/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+  ).then((response) => {
+    if (!response.ok) {
+      return null;
+    }
+    return response.json();
+  }).catch((error) => {
+    throw error;
+  });
+};
+
+export const getTvCredits = (id) => {
+  return fetch(
+    `${tmdbUrl}/tv/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+  ).then((response) => {
+    if (!response.ok) {
+      return null;
+    }
+    return response.json();
+  }).catch((error) => {
+    throw error;
+  });
+};
+
+export const getPeopleCredits = (id) => {
+  return fetch(
+    `${tmdbUrl}/person/${id}/combined_credits?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      return null;
+    }
+    return response.json();
+  }).catch((error) => {
+    throw error;
+  });
+};
+
+export const getPopularPeople = () => {
+  return fetch(
+    `${tmdbUrl}/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      return null;
+    }
+    return response.json();
+  }).catch((error) => {
+    throw error;
+  });
+};
